e4e5464da2c79412501e42c79d797e2ac15531fc
CiceroSousa <36247510+CiceroSousa@users.noreply.github.com>
2018-03-08 20:13:46 -0300

    Delete livro.h

--- 07.03.2018/livro.h
+++
@@ -1,5 +0,0 @@
-typedef struct livro Livro;
-
-Livro* criar(char *titulo, int ano, int tot_pag, float preco);
-void acessar(Livro *livro, char *titulo, int *ano, int *tot_pag, float *preco);
-void liberar(Livro *livro);
