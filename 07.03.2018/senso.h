3aefc96ffd52dce8fa9bf94e0c151b469c6e17ed
CiceroSousa <36247510+CiceroSousa@users.noreply.github.com>
2018-03-08 20:13:54 -0300

    Delete senso.h

--- 07.03.2018/senso.h
+++
@@ -1,6 +0,0 @@
-typedef struct senso Senso;
-
-Senso* criar(char *sexo, int idade, float salario, int quant_filho);
-void acessar(Senso *senso, char *sexo, int *idade, float *salario, int *quant_filho);
-void liberar(Senso *senso);
-
